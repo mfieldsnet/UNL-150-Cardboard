bef23a09257c9b2906886dfed7091bb506e77867
jake <linsijing@hotmail.com>
2019-01-04 10:14:13 -0600

    add script to enable player motion

--- Assets/script/PlayerMotion.cs
+++ Assets/script/PlayerMotion.cs
@@ -4,22 +4,26 @@ using UnityEngine.UI;
 
 public class PlayerMotion : MonoBehaviour
 {
+	int back;
     Rigidbody m_Rigidbody;
     public float m_Speed = 5.0f;
 
     void Start()
     {
         //Fetch the Rigidbody component you attach from your GameObject
-        m_Rigidbody = gameObject.AddComponent<Rigidbody>();//GetComponent<Rigidbody>();
-        m_Rigidbody.drag = 5.0f;
+        //m_Rigidbody = gameObject.AddComponent<Rigidbody>();//GetComponent<Rigidbody>();
+        //m_Rigidbody.drag = 5.0f;
     }
 
     void Update()
     {
-        if (Input.touchCount > 0)
+        if (Input.GetButton("Fire1"))
         {
-            m_Rigidbody.velocity = new Vector3(transform.forward.x, 0, transform.forward.z) * m_Speed;
-            Debug.Log(m_Rigidbody.name);
+        	back = Camera.main.transform.rotation.x < 0.20 ? 1 : -1;
+        	//move back when player looks down by some degree which I'm not sure to the horizontal
+        	
+        	//Debug.Log(back + " " + Camera.main.transform.rotation.x);
+            transform.position = transform.position + back * (Camera.main.transform.forward * m_Speed * Time.deltaTime);
         }
     }
 }
