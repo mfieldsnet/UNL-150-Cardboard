020bd19a54b38cc68f01c0708609231672ba87a2
jake <linsijing@hotmail.com>
2019-01-22 23:16:09 -0600

    show text when touch spheres

---
+++ Assets/script/ShowUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+    when player hit the cube, the canvas shows up
+*/
+public class ShowUI : MonoBehaviour {
+
+    public GameObject[] uiObject;
+    void Start()
+    {
+    	for (int i = 0; i < uiObject.Length; i++) {
+    		uiObject[i].transform.GetChild(1).gameObject.SetActive(false);
+    	}
+    }
+	// Update is called once per frame
+	void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "uiObject")
+        {
+        	collision.gameObject.SetActive(false);
+            collision.gameObject.transform.parent.transform.GetChild(1).gameObject.SetActive(true);
+            StartCoroutine(WaitForSec(collision));
+        }
+	}
+    IEnumerator WaitForSec(Collision collision)
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(collision.gameObject.transform.parent.gameObject);
+    }
+    
+}
